2e4e5fcf871c146fa3a349f9ad3788a91f1bf5d8
BogomolDmitriy <bogomoldima1992@gmail.com>
2021-11-19 00:06:11 +0200

    Add project files.

---
+++ Lesson 7 multidimensional arrays/Program.cs
@@ -0,0 +1,55 @@
+﻿using LibraryLesson7;
+using System;
+
+namespace Lesson_7_multidimensional_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput;
+            do
+            {
+                userInput = Convert.ToInt32(Console.ReadLine());
+                switch(userInput)
+                {
+                    case 1:
+                        {
+
+
+                            Console.WriteLine($"Task№1");
+                            Console.Write("Enter Y=");
+                            int Y = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter X=");
+                            int X = Convert.ToInt32(Console.ReadLine());
+                            Console.Write($"{Environment.NewLine}Minimum value = {LibraryArr.Task1(Y, X)}");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine($"Task№2");
+                            Console.Write("Enter Y=");
+                            int Y = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter X=");
+                            int X = Convert.ToInt32(Console.ReadLine());
+                            Console.Write($"{Environment.NewLine}Maximum value = {LibraryArr.Task2(Y, X)}");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine($"Task№3");
+                            Console.Write("Enter Y=");
+                            int Y = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter X=");
+                            int X = Convert.ToInt32(Console.ReadLine());
+
+                            (int column, int line) = LibraryArr.Task3(Y, X);
+                            break;
+                        }
+                }
+            }
+
+            while (userInput != 0);
+        }
+    }
+}
---
+++ LibraryLesson7/LibraryArr.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace LibraryLesson7
+{
+    public class LibraryArr
+    {
+        public static int Task1(int Y, int X)
+        {
+            int[,] arr = new int[Y, X];
+            Random random = new Random();
+            for (int i=0; i < arr.GetLength(0); i++)
+            {
+                for (int j=0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(1000);
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int min = arr[0, 1];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i,j]<min)
+                    {
+                        min = arr[i, j];
+                    }
+                }
+            }
+
+            return min;
+        }
+
+        public static int Task2(int Y, int X)
+        {
+            int[,] arr = new int[Y, X];
+            Random random = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(1000);
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int max = arr[0, 1];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > max)
+                    {
+                        max = arr[i, j];
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        public static (int column, int line) Task3(int Y, int X)
+        {
+            int[,] arr = new int[Y, X];
+            Random random = new Random();
+            int line=0;
+            int column=0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(1000);
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int min = arr[0, 1];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] < min)
+                    {
+                        min = arr[i, j];
+                        column = i;
+                        line = j;
+                    }
+                }
+            }
+
+            return column, line;
+        }
+
+    }
+}
