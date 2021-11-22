35b74abeadd61df506568cfffa3a0ddf36955a1a
BogomolDmitriy <bogomoldima1992@gmail.com>
2021-11-22 20:20:38 +0200

    22.11.2021 Task5

--- Lesson 7 multidimensional arrays/Program.cs
+++ Lesson 7 multidimensional arrays/Program.cs
@@ -25,6 +25,7 @@ namespace Lesson_7_multidimensional_arrays
                             Console.Write($"{Environment.NewLine}Minimum value = {LibraryArr.Task1(Y, X)}");
                             break;
                         }
+
                     case 2:
                         {
                             Console.WriteLine($"Task№2");
@@ -35,6 +36,7 @@ namespace Lesson_7_multidimensional_arrays
                             Console.Write($"{Environment.NewLine}Maximum value = {LibraryArr.Task2(Y, X)}");
                             break;
                         }
+
                     case 3:
                         {
                             Console.WriteLine($"Task№3");
@@ -44,8 +46,36 @@ namespace Lesson_7_multidimensional_arrays
                             int X = Convert.ToInt32(Console.ReadLine());
 
                             (int column, int line) = LibraryArr.Task3(Y, X);
+
+                            Console.WriteLine($"X= {line}{Environment.NewLine}Y= {column}");
                             break;
                         }
+
+                    case 4:
+                        {
+                            Console.WriteLine($"Task№4");
+                            Console.Write("Enter Y=");
+                            int Y = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter X=");
+                            int X = Convert.ToInt32(Console.ReadLine());
+
+                            (int column, int line) = LibraryArr.Task4(Y, X);
+                            Console.WriteLine($"X= {line}{Environment.NewLine}Y= {column}");
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            Console.WriteLine($"Task№4");
+                            Console.Write("Enter Y=");
+                            int Y = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Enter X=");
+                            int X = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine(LibraryArr.Task5(Y, X));
+                            break;
+                        }
+
                 }
             }
 
--- LibraryLesson7/LibraryArr.cs
+++ LibraryLesson7/LibraryArr.cs
@@ -8,9 +8,9 @@ namespace LibraryLesson7
         {
             int[,] arr = new int[Y, X];
             Random random = new Random();
-            for (int i=0; i < arr.GetLength(0); i++)
+            for (int i = 0; i < arr.GetLength(0); i++)
             {
-                for (int j=0; j < arr.GetLength(1); j++)
+                for (int j = 0; j < arr.GetLength(1); j++)
                 {
                     arr[i, j] = random.Next(1000);
                     Console.Write(arr[i, j] + "\t");
@@ -23,7 +23,7 @@ namespace LibraryLesson7
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                    if (arr[i,j]<min)
+                    if (arr[i, j] < min)
                     {
                         min = arr[i, j];
                     }
@@ -66,8 +66,8 @@ namespace LibraryLesson7
         {
             int[,] arr = new int[Y, X];
             Random random = new Random();
-            int line=0;
-            int column=0;
+            int line = 0;
+            int column = 0;
             for (int i = 0; i < arr.GetLength(0); i++)
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
@@ -78,7 +78,7 @@ namespace LibraryLesson7
                 Console.WriteLine();
             }
 
-            int min = arr[0, 1];
+            int min = arr[0, 0];
             for (int i = 0; i < arr.GetLength(0); i++)
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
@@ -92,8 +92,188 @@ namespace LibraryLesson7
                 }
             }
 
-            return column, line;
+            return (column, line);
         }
 
+        public static (int column, int line) Task4(int Y, int X)
+        {
+            int[,] arr = new int[Y, X];
+            Random random = new Random();
+            int line = 0;
+            int column = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(1000);
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int max = arr[0, 0];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > max)
+                    {
+                        column = i;
+                        line = j;
+                    }
+                }
+            }
+
+            return (column, line);
+
+        }
+        public static int Task5(int Y, int X)
+        {
+            int[,] arr = new int[Y, X];
+            Random random = new Random();
+            int counter = 0;
+            int more = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(1000);
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    counter = 0;
+                    if (i - 1 >= 0 && j - 1 >= 0)
+                    {
+                        if (arr[i, j] > arr[i - 1, j - 1])
+                        {
+                            counter = counter + 1;
+                        }    
+                    }
+
+                    else
+                    {
+                        counter = counter + 1;
+                    }
+
+                    if (i - 1 >= 0)
+                    {
+                        if (arr[i, j] > arr[i - 1, j])
+                        {
+                            counter = counter + 1;
+                        }
+                    }
+
+                    else
+                    {
+                        counter = counter + 1;
+                    }
+
+                    if (i - 1 >= 0)
+                    {
+                        if (arr[i, j] > arr[i - 1, j])
+                        {
+                            counter = counter + 1;
+                        }
+                    }
+
+                    else
+                    {
+                        counter = counter + 1;
+                    }
+
+                    if (i - 1 >= 0 && j + 1 <= arr.GetLength(1))
+                    {
+                        if (arr[i, j] > arr[i - 1, j + 1])
+                        {
+                            counter = counter + 1;
+                        }
+                    }
+
+                    else
+                    {
+                        counter = counter + 1;
+                    }
+
+                    if (j - 1 >= 0)
+                    {
+                        if (arr[i, j] > arr[i, j - 1])
+                        {
+                            counter = counter + 1;
+                        }
+                    }
+
+                    else
+                    {
+                        counter = counter + 1;
+                    }
+
+                    if (j + 1 <= arr.GetLength(1))
+                    {
+                        if (arr[i, j] > arr[i, j + 1])
+                        {
+                            counter = counter + 1;
+                        }
+                    }
+
+                    else
+                    {
+                        counter = counter + 1;
+                    }
+
+                    if (i + 1 <= arr.GetLength(0) && j - 1 >= 0)
+                    {
+                        if (arr[i, j] > arr[i + 1, j - 1])
+                        {
+                            counter = counter + 1;
+                        }
+                    }
+
+                    else
+                    {
+                        counter = counter + 1;
+                    }
+
+                    if (i + 1 <= arr.GetLength(0))
+                    {
+                        if (arr[i, j] > arr[i + 1, j])
+                        {
+                            counter = counter + 1;
+                        }
+                    }
+
+                    else
+                    {
+                        counter = counter + 1;
+                    }
+
+                    if (i + 1 <= arr.GetLength(0) && j + 1 <= arr.GetLength(1))
+                    {
+                        if (arr[i, j] > arr[i + 1, j + 1])
+                        {
+                            counter = counter + 1;
+                        }
+                    }
+
+                    else
+                    {
+                        counter = counter + 1;
+                    }
+
+                    if (counter == 8)
+                    {
+                        more = more + 1;
+                    }
+
+                }
+            }
+
+            return (more);
+        }
     }
 }
